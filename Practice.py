adf46496a3632b45a1cbc63d3e57fef55f9217af
Shivaninaroju <shivaninaroju1@gmail.com>
2025-02-04 17:01:51 +0530

    Modified

--- Practice.py
+++ Practice.py
@@ -1,6 +1,18 @@
-##Day 1
+
 class Student:
     """This class is developed by Shivani for Demo purpose"""
     
 print(Student.__doc__)
 help(Student)
+ #Example
+class Student:
+    def __init__(self):
+        self.name="Shivani"
+        self.age=19
+        self.rno=204
+    def talk(self):
+        print("My name is:",self.name)
+        print("My age is:",self.age)
+        print("My rno is:",self.rno)
+s=Student()
+s.talk()
