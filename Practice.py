ddd543f59ecb24ea676c55bf1455bc285d2a0f2b
Shivaninaroju <shivaninaroju1@gmail.com>
2025-02-07 15:26:27 +0530

    Modified

--- Practice.py
+++ Practice.py
@@ -1,4 +1,4 @@
-#Welcome 
+#Day-1 
 class Student:
     """This class is developed by Shivani for Demo purpose"""
     
@@ -25,3 +25,21 @@ s2=Student()
 #Constructor is executed 2 times beacause of two different objects
 print(id(s1))
 print(id(s2))
+
+#Day-2
+##To execute multiple students information
+class Student:
+    def __init__(self,name,age,rno):
+        self.name=name
+        self.age=age
+        self.rno=rno
+    def talk(self):
+        print("Name:",self.name)
+        print("Age:",self.age)
+        print("Rno:",self.rno)
+s1=Student("shivani",19,204)
+s2=Student("Niveditha",18,205)
+s3=Student("Kavya",18,206)
+s1.talk()
+s2.talk()
+s3.talk()
